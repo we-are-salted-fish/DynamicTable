e1815ab4e6768876059b1fb5dabb63478428bcee
无语 <quanyuqiu@gmail.com>
2021-03-12 14:51:19 +0800

    update dll
    更新版本

---
+++ src/DynamicTable/Models/ColumnConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace DynamicTable.Models
+{
+    public class ColumnConfig
+    {
+        public string HeadText { get; set; }
+
+        public string Name { get; set; }
+
+        public string DataBaseColumnName { get; set; }
+        
+        public CtrlType CtrlType { get; set; }
+        
+        
+    }
+}
---
+++ src/DynamicTable/Models/TableConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace DynamicTable.Models
+{
+    public class TableConfig
+    {
+        public string Table { get; set; }
+        
+        public List<ColumnConfig> Columns { get; set; }
+    }
+}
